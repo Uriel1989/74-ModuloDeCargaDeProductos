782f4b3023f7ca2b60a0fd4cdb471c3f07984a97
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:36:49 -0300

    Add files via upload

---
+++ 74-ModuloDeCargaDeProductos/ModuloDeCargaDeProductos/CargaDeDatos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModuloDeCargaDeProductos
+{
+    class CargaDeDatos
+    {
+        public string TipoDispositivo { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set;  }
+        public int NumSerie { get; set; }
+        public int Precio { get; set; }
+
+        public string ListadoPorTipo()
+        {
+            return "Listado por tipo: " + " Marca->" + this.Marca + "-Modelo-> " + this.Modelo;
+        }
+
+        public string ListadoCompleto()
+        {
+            return "Listado completo: " + "Tipo de Dispositivo->"+this.TipoDispositivo+"-Marca->" + this.Marca + "-Modelo-> " + this.Modelo + "-Precio->" + this.Precio;
+        }
+    }
+}
---
+++ 74-ModuloDeCargaDeProductos/ModuloDeCargaDeProductos/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModuloDeCargaDeProductos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorLeft = 30;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Modulo de carga de datos");
+
+            Console.WriteLine("Informe la cantidad de dispositivos que desea ingresar:");
+
+            int Cantidad = Convert.ToInt32(Console.ReadLine());
+
+            List<CargaDeDatos> ListadoTipos = new List<CargaDeDatos>();
+
+            List<CargaDeDatos> ListadoCompleto = new List<CargaDeDatos>();
+
+            for (int i=0; i<Cantidad; i++)
+            {
+                CargaDeDatos Producto = new CargaDeDatos();
+
+                Console.WriteLine("Ingrese el tipo de dispositivo (mother, procesador, memoroia, fuente, gabinete, etc.)");
+
+                Producto.TipoDispositivo = Console.ReadLine();
+
+                Console.WriteLine("Ingrese la marca:");
+
+                Producto.Marca = Console.ReadLine();
+
+                Console.WriteLine("Ingrese el modelo:");
+
+                Producto.Modelo = Console.ReadLine();
+
+                Console.WriteLine("Ingrese el numero de serie: ");
+
+                Producto.NumSerie = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el precio:");
+
+                Producto.Precio = Convert.ToInt32(Console.ReadLine());
+
+                ListadoTipos.Add(Producto);
+
+                ListadoCompleto.Add(Producto);
+            }
+
+            Console.WriteLine("Ingrese el listado que desea ver: 'TIPO' o 'COMPLETO':");
+
+            string acceso = Console.ReadLine();
+
+            switch (acceso.ToUpper())
+            {
+                case "TIPO":
+                    Console.WriteLine("Listado por tipo:");
+                    for (int i = 0; i < ListadoTipos.Count; i++)
+                    {
+                        CargaDeDatos j = ListadoTipos[i];
+                        Console.WriteLine(j.ListadoPorTipo());
+                    }
+                    break;
+                case "COMPLETO":
+                    Console.WriteLine("Listado completo:");
+                    for (int i = 0; i < ListadoCompleto.Count; i++)
+                    {
+                        CargaDeDatos j = ListadoCompleto[i];
+                        Console.WriteLine(j.ListadoCompleto());
+                    }
+                    break;
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
